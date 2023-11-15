eaca19c4b8acd1a78a3b88435102e06fad8b3617
Dehorla-Tech <dehorladesign0001:gmail.com>
2023-11-15 20:43:51 +0100

    modified

--- twoSum.js
+++ twoSum.js
@@ -3,11 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    for(var i = 0; i < nums.length; i++){
-            var x = nums[i];
-       for(var j = i + 1; j < nums.length; j++){
-                var y = nums[j];
+const twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+            let x = nums[i];
+       for(let j = i + 1; j < nums.length; j++){
+                let y = nums[j];
                if (target === x + y){
                        return [i, j]
                }
